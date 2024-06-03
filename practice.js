b65bdf1902b00c458fe60ad5788c05d9d4b02929
VIVIAN BASSEY <vivianbasseya@gmail.com>
2024-06-03 14:36:56 +0100

    y

--- practice.js
+++ practice.js
@@ -1,5 +1,14 @@
 function checks(){
     return new Promise((resolve,reject)=>{
-        numbers= Math.flo
+        numbers= Math.floor(Math.random()*6)
+        console.log("number is: ",numbers)
+        if (numbers>=3){
+            console.log("sucess number")
+            resolve()
+        }else{
+            console.log("failed number")
+            reject()
+        }
     })
 }
+ checks()
